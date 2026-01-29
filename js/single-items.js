1c16ec41ef917f2e1742d59a74b551f79f32dfe3
Loblesh bhartal <lovelessbhartal12@gmail.com>
2026-01-29 16:14:56 +0545

    add delete features

--- js/single-items.js
+++ js/single-items.js
@@ -16,15 +16,12 @@ export function createSingleItem(item) {
       <i class="fa-regular fa-trash-can"></i>
     </button>`;
 
-  // Add event listener for checkbox
   const checkbox = div.querySelector('input[type="checkbox"]');
   checkbox.addEventListener("change", () => editCompleted(item.id));
 
-  // Add event listener for edit button
   const editBtn = div.querySelector(".edit-btn");
   editBtn.addEventListener("click", () => editItem(item.id));
 
-  // Add event listener for remove button
   const removeBtn = div.querySelector(".remove-btn");
   removeBtn.addEventListener("click", () => removeItem(item.id));
 

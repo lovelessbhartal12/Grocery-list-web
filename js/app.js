9ec815584281dae442a92255550dd4461f612f65
Loblesh bhartal <lovelessbhartal12@gmail.com>
2026-01-30 08:17:10 +0545

    items add features

--- js/app.js
+++ js/app.js
@@ -1,13 +1,15 @@
 import { groceryItems } from "./data.js";
 import { createItems } from "./items.js";
-
+import { createForm } from "./form.js";
 let items = groceryItems;
 
 function render() {
   const app = document.getElementById("app");
   app.innerHTML = "";
+  const formElement = createForm();
 
   const itemsElement = createItems(items);
+  app.append(formElement);
   app.appendChild(itemsElement);
 }
 
@@ -16,4 +18,18 @@ export function removeItem(itemId) {
   items = items.filter((item) => item.id !== itemId);
   render();
   setTimeout(() => alert("Item Deleted Successfully!"), 0);
+}
+function generateId() {
+  return Date.now().toString(36) + Math.random().toString(36).substr(2);
+}
+
+export function addItem(itemName) {
+  const newItem = {
+    name: itemName,
+    completed: false,
+    id: generateId(),
+  };
+  items = [...items, newItem];
+  render();
+  setTimeout(() => alert("Item Added Successfully!"), 0);
 }
